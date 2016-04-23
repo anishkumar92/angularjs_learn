0a1f36cdb7e6aa18f1207096253c4e9b7c6d53f5
anishkumar92 <anishkumar92@gmail.com>
2016-04-24 03:04:17 +0530

    profile

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,20 @@
    var myapp = angular.module("myApp", []);
+           
             myapp.controller("myCntrl", function ($scope) {
                 $scope.first=0;
                 $scope.second=1;
 
                 $scope.updateValue = function(){
                 	$scope.test = (+$scope.first + +$scope.second);
+            };
+            });
+
+             myapp.controller("two", function ($scope) {
+                $scope.first=0;
+                $scope.second=1;
+
+                $scope.updateValue = function(){
+                	$scope.test = (+$scope.first + +$scope.second);
+                	
             };
             });
